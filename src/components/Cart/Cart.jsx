a71aa36ece89cd807070c0c0dfbfa3c6dc09c302
Colum Kelly <columk1@users.noreply.github.com>
2023-11-07 00:10:16 -0800

    Feat: Increment and decrement buttons in shopping cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,14 +3,24 @@ import CartItem from '../CartItem/CartItem'
 import { useOutletContext } from 'react-router-dom'
 
 const Cart = () => {
-  const { cartItems, setCartItems } = useOutletContext()
   console.log('Cart Renders')
+  const { cartItems, setCartItems } = useOutletContext()
+
+  const updateQuantity = (id, newQuantity) => {
+    if (newQuantity < 0) return
+    const newCartItems = cartItems.map((item) => {
+      return item.id === id ? { ...item, quantity: newQuantity } : item
+    })
+    console.log(newCartItems)
+    setCartItems(newCartItems)
+  }
+
   return (
     <section className={styles.cart}>
       <h2 className={styles.title}>Shopping Cart</h2>
       <div className={styles.cartItems}>
         {cartItems.map((item) => (
-          <CartItem key={item.id} product={item} />
+          <CartItem key={item.id} product={item} onChange={updateQuantity} />
         ))}
       </div>
       <button className={styles.checkoutBtn}>Checkout</button>
--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,6 +1,9 @@
 import styles from './CartItem.module.css'
 
-const CartItem = ({ product }) => {
+const CartItem = ({ product, onChange }) => {
+  const increment = () => onChange(product.id, product.quantity + 1)
+  const decrement = () => onChange(product.id, product.quantity - 1)
+
   return (
     <div className={styles.item}>
       <button className={styles.deleteBtn}>x</button>
@@ -12,9 +15,9 @@ const CartItem = ({ product }) => {
         <span>${product.price}</span>
       </div>
       <div className={styles.quantity}>
-        <button>-</button>
-        <input type='text' name='quantity' value='1'></input>
-        <button>+</button>
+        <button onClick={decrement}>-</button>
+        <input type='text' name='quantity' value={product.quantity}></input>
+        <button onClick={increment}>+</button>
       </div>
       <div className={styles.totalPrice}>${product.price}</div>
     </div>
